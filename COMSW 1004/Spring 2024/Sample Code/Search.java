88665b69a71a9a014eaf4e539e20ade846f9fa7c
Griffin Newbold <46826674+griffinnewbold@users.noreply.github.com>
2024-01-18 12:17:41 -0500

    Create Search.java

---
+++ COMSW 1004/Spring 2024/Sample Code/Search.java
@@ -0,0 +1,38 @@
+public class Search {
+
+  //Runs the sequential search algorithm and returns true or false.
+  public static boolean seqeuntialSearch(int[] a, int x) {
+    int i = 0;
+    while (i < a.length) {
+      if (a[i] == x) {
+        return true;
+      }
+      i += 1;
+    }
+    return false;
+  }
+
+  //Runs the binary search algorithm and returns true or false.
+  public static boolean binarySearch(int[] a, int x) {
+    int leftPointer = 0;
+    int rightPointer = a.length - 1;
+
+    while (leftPointer <= rightPointer) {
+      int mid = (leftPointer + rightPointer) / 2;
+      if (a[mid] == x) {
+        return true;
+      } else if (a[mid] < x) {
+        leftPointer = mid + 1;
+      } else {
+        rightPointer = mid - 1;
+      }
+    }
+    return false;
+  }
+
+  public static void main(String[] args) {
+    int[] a = {1,2,3,4,5,6,7,8};
+    System.out.println(binarySearch(a, 7));
+    System.out.println(sequentialSearch(a, 9));
+  }
+}
