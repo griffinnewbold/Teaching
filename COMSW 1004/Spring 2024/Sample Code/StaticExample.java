1e831ab3a581fd1215b2aa4d6cf44df77749a000
Griffin Newbold <46826674+griffinnewbold@users.noreply.github.com>
2024-02-27 18:21:16 -0500

    Update StaticExample.java

--- COMSW 1004/Spring 2024/Sample Code/StaticExample.java
+++ COMSW 1004/Spring 2024/Sample Code/StaticExample.java
@@ -1,18 +1,25 @@
 public class StaticExample {
 
+  //This is a static attribute, it is shared among all instances of the class 
+  //if one instance alters the value of the member, all instances have that 
+  //altered value.
   private static int sharedNumber = 0;
+
+  //standard instance attribute.
   private int myNumber;
 
-
+  //standard constructor
   public StaticExample() { 
     myNumber = sharedNumber;
     sharedNumber += 2;
   }
 
+  //this method is static, it does not need an object reference to be called
   public static int getSharednumber() {
     return sharedNumber;
   }
 
+  //standard accessor
   public int getMyNumber() {
     return myNumber;
   }
