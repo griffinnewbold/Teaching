8901db876f1c8e5793538cfc7286ec36e44e6275
Griffin Newbold <46826674+griffinnewbold@users.noreply.github.com>
2024-03-17 00:03:24 -0400

    update

--- COMSW 1004/Spring 2024/Sample Code/Sample.java
+++ COMSW 1004/Spring 2024/Sample Code/Sample.java
@@ -4,6 +4,7 @@ public class Sample {
   public static void main(String[] args){
     System.out.println("sample");
     int x = 5;
+    int c = x;
     String y = "hello";
     System.out.println(y);
   }
