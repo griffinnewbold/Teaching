20a7c3533d20ce91d45f23fef94b86659a309776
Griffin Newbold <46826674+griffinnewbold@users.noreply.github.com>
2024-05-10 15:01:39 -0400

    Update HelloWorld.java

--- COMSW 1004/Spring 2024/Sample Code/HelloWorld.java
+++ COMSW 1004/Spring 2024/Sample Code/HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
 
+  //this is the main method which is the entry point for a java program
   public static void main(String[] args){
     System.out.println("Hello World!");
   }
